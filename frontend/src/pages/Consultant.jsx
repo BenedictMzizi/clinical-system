ffcec1bf5ef80cab20cf2ad6845b8ef9c0aacd8d
BenedictMzizi <61235432+BenedictMzizi@users.noreply.github.com>
2026-06-22 20:48:16 +0200

    Update Consultant.jsx

--- frontend/src/pages/Consultant.jsx
+++ frontend/src/pages/Consultant.jsx
@@ -299,7 +299,7 @@ if (validMedications.length > 0)
 
        prescription_number: prescriptionNumber,
 
-       pharmacist_note: null,
+       pharmacist_note: pharmacy_note?.trim() || null
 
        status: PrescriptionStatus.PENDING,
 
