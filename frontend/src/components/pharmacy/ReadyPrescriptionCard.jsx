565b7953671a05d5265629bb3417d2dc2859d24a
BenedictMzizi <61235432+BenedictMzizi@users.noreply.github.com>
2026-07-08 16:04:44 +0200

    Add ReadyPrescriptionCard component for prescriptions

--- frontend/src/components/pharmacy/ReadyPrescriptionCard.jsx
+++ frontend/src/components/pharmacy/ReadyPrescriptionCard.jsx
@@ -1,1 +1,103 @@
+import React from "react";
 
+import {
+
+    card,
+
+    buttonPrimary
+
+} from "../../styles/styles";
+
+import {
+
+    formatMedications
+
+} from "../../utils/medicationUtils";
+
+export default function ReadyPrescriptionCard({
+
+    prescription,
+
+    onCollect
+
+}){
+
+    return(
+
+        <div
+
+            style={{
+
+                ...card,
+
+                border:"2px solid green"
+
+            }}
+
+        >
+
+            <strong>
+
+                {
+
+                    prescription.patient?.full_name
+
+                }
+
+            </strong>
+
+            <div>
+
+                {
+
+                    prescription.patient?.id_number
+
+                }
+
+            </div>
+
+            <div style={{marginTop:10}}>
+
+                {
+
+                    formatMedications(
+
+                        prescription.medications
+
+                    )
+
+                }
+
+            </div>
+
+            <button
+
+                style={{
+
+                    ...buttonPrimary,
+
+                    marginTop:15
+
+                }}
+
+                onClick={()=>
+
+                    onCollect(
+
+                        prescription
+
+                    )
+
+                }
+
+            >
+
+                Hand To Patient
+
+            </button>
+
+        </div>
+
+    );
+
+}
